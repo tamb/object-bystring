901d5e085549f1c84fa38f8916ef825430ff5676
tamb <t.saporito@gmail.com>
2019-09-19 17:24:34 -0400

    make code readable

--- src/polyfill.js
+++ src/polyfill.js
@@ -4,33 +4,33 @@ if (!Object.prototype.byString) {
     enumerable: false,
     configurable: false,
     writable: false,
-    value: function(s, v, o) {
-      var _o = o || this;
-      s = s.toString().replace(/\[(\w+)\]/g, ".$1"); // CONVERT INDEXES TO PROPERTIES
-      s = s.toString().replace(/^\./, ""); // STRIP A LEADING DOT
-      var a = s.split("."); //ARRAY OF STRINGS SPLIT BY '.'
-      for (var i = 0; i < a.length; ++i) {
+    value: function(string, value, object) {
+      var _object = object || this;
+      string = string.toString().replace(/\[(\w+)\]/g, ".$1"); // CONVERT INDEXES TO PROPERTIES
+      string = string.toString().replace(/^\./, ""); // STRIP A LEADING DOT
+      var arrayOfStrings = string.split("."); //ARRAY OF STRINGS SPLIT BY '.'
+      for (var i = 0; i < arrayOfStrings.length; ++i) {
         //LOOP OVER ARRAY OF STRINGS
-        var k = a[i];
-        if (k in _o) {
+        var key = arrayOfStrings[i];
+        if (key in _object) {
           //LOOP THROUGH OBJECT KEYS
-          if (_o.hasOwnProperty(k)) {
+          if (_object.hasOwnProperty(key)) {
             //USE ONLY KEYS WE CREATED
-            if (v !== undefined) {
+            if (value !== undefined) {
               //IF WE HAVE A NEW VALUE PARAM
-              if (i === a.length - 1) {
+              if (i === arrayOfStrings.length - 1) {
                 //IF IT'S THE LAST IN THE ARRAY
-                _o[k] = v;
+                _object[key] = value;
               }
             }
-            _o = _o[k]; //NO NEW VALUE SO JUST RETURN THE CURRENT VALUE
+            _object = _object[key]; //NO NEW VALUE SO JUST RETURN THE CURRENT VALUE
           }
         } else {
           //
-          _o[k] = v; //
+          _object[key] = value; //
         } //Added 1.18.18 so that objects that have no keys will simply have this key added to it
       }
-      return _o;
+      return _object;
     }
   });
 }
