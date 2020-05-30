1d346874f63f01a227f34cf771d4755d18f8c2be
Tom Saporito <tamb89@protonmail.com>
2020-05-30 10:59:26 -0400

    remove dist

--- dist/index.js
+++
@@ -1,1 +0,0 @@
-!function(e){"function"==typeof define&&define.amd?define(e):e()}((function(){"use strict";module.exports=function(e,n,t){for(var i=e,r=(n=(n=n.toString().replace(/\[(\w+)\]/g,".$1")).toString().replace(/^\./,"")).split("."),o=0;o<r.length;++o){var f=r[o];f in i?i.hasOwnProperty(f)&&(void 0!==t&&o===r.length-1&&(i[f]=t),i=i[f]):i[f]=t}return i}}));
