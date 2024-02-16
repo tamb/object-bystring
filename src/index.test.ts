71f53d009804557d724486b29cbc961b2dc1e3fa
tamb <t.saporito@gmail.com>
2024-02-15 23:14:03 -0500

    Adds more tests and cleans up mocks

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,50 +1,70 @@
 import byString from "./index";
 import objectToMock from "./mocks";
 
+let data: any;
+
 describe("byString", () => {
+  beforeEach(() => {
+    data = objectToMock;
+  });
+
   test("byString to be a function", () => {
     expect(byString).toBeInstanceOf(Function);
   });
 
   test("Reads string from object", () => {
-    expect(byString(objectToMock, "foo")).toBe("bar");
+    expect(byString(data, "foo")).toBe("bar");
   });
 
   test("writes new value to object", () => {
-    expect(objectToMock.buzz).toBeUndefined();
-    byString(objectToMock, "buzz", "fizz");
-    expect(objectToMock.buzz).toBe("fizz");
+    expect(data.buzz).toBeUndefined();
+    byString(data, "buzz", "fizz");
+    expect(data.buzz).toBe("fizz");
   });
 
   test("String gets variable from object", () => {
-    const firstName = byString(objectToMock, "name.first");
+    const firstName = byString(data, "name.first");
     expect(firstName).toBe("John");
   });
 
   test("String sets variable in object", () => {
-    byString(objectToMock, "name.first", "James");
-    expect(objectToMock.name.first).toBe("James");
+    byString(data, "name.first", "James");
+    expect(data.name.first).toBe("James");
   });
 
   test("String sets variable one deep", () => {
-    byString(objectToMock, "people[0]", "Jeremy");
-    if (objectToMock.people) {
-      expect(objectToMock.people[0]).toMatch("Jeremy");
+    byString(data, "people[0]", "Jeremy");
+    if (data.people) {
+      expect(data.people[0]).toMatch("Jeremy");
     }
   });
 
   test("String sets variable two deep", () => {
-    byString(objectToMock, "place.address.city", "Chicago");
-    expect(objectToMock.place.address.city).toMatch("Chicago");
+    byString(data, "place.address.city", "Chicago");
+    expect(data.place.address.city).toMatch("Chicago");
   });
 
   test("Adds value to object", () => {
-    byString(objectToMock, "place.address.county", "Cook");
-    expect(objectToMock.place.address.county).toMatch("Cook");
+    expect(data.place.address.county).toBeUndefined();
+    byString(data, "place.address.county", "Cook");
+    expect(data.place.address.county).toMatch("Cook");
   });
 
   test("Sets date to object", () => {
-    byString(objectToMock, "place.address.date", new Date());
-    expect(objectToMock.place.address.date).toBeInstanceOf(Date);
+    byString(data, "place.address.date", new Date());
+    expect(data.place.address.date).toBeInstanceOf(Date);
+  });
+
+  test("Adds value to array", () => {
+    expect(data.people).toHaveLength(1);
+    byString(data, "people[1]", "Jill");
+    expect(data.people).toHaveLength(2);
+    expect(data.people[1]).toMatch("Jill");
+  });
+
+  test("Sets value to array", () => {
+    byString(data, "people[2]", "Jill");
+    // @ts-ignore
+    expect(data.people[2]).toMatch("Jill");
   });
 });
