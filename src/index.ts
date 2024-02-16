f7652fbb0c59eb70e31bc1081fd283c2253977c6
tamb <t.saporito@gmail.com>
2024-02-15 23:47:29 -0500

    uses new implementation

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,34 @@
-/**
- *
- * Thank you, Ray for the original Stackoverflow answer, which is the majority of the source code.
- * https://stackoverflow.com/questions/6491463/accessing-nested-javascript-objects-with-string-key
- * Check out his github and so profile below:
- * https://github.com/raybellis
- * https://stackoverflow.com/users/6782/alnitak
- *
- */
+export default function byString(obj: any, key: string, value?: any): any {
+  const keys = key.split(/\.|\[(\d+)\]/).filter(Boolean);
+  let currentObj = obj;
 
-export default function byString(object: object, string: string, value?: any) {
-  var _object: any = object;
-  string = string.toString().replace(/\[(\w+)\]/g, ".$1"); // CONVERT INDEXES TO PROPERTIES
-  string = string.toString().replace(/^\./, ""); // STRIP A LEADING DOT
-  var arrayOfStrings = string.split("."); //ARRAY OF STRINGS SPLIT BY '.'
-  for (var i = 0; i < arrayOfStrings.length; ++i) {
-    //LOOP OVER ARRAY OF STRINGS
-    var key: any = arrayOfStrings[i];
-    if (_object[key]) {
-      //LOOP THROUGH OBJECT KEYS
-      if (_object.hasOwnProperty(key)) {
-        //USE ONLY KEYS WE CREATED
-        if (value !== undefined) {
-          //IF WE HAVE A NEW VALUE PARAM
-          if (i === arrayOfStrings.length - 1) {
-            //IF IT'S THE LAST IN THE ARRAY
-            _object[key] = value;
-          }
-        }
-        _object = _object[key]; //NO NEW VALUE SO JUST RETURN THE CURRENT VALUE
+  for (let i = 0; i < keys.length - 1; i++) {
+    const currentKey = keys[i];
+    // If the current key is a number, treat it as an array index
+    if (/^\d+$/.test(keys[i + 1])) {
+      if (!Array.isArray(currentObj[currentKey])) {
+        currentObj[currentKey] = [];
       }
     } else {
-      //
-      _object[key] = value; //
-    } //Added 1.18.18 so that objects that have no keys will simply have this key added to it
+      if (!currentObj[currentKey]) {
+        // Check if the next key is a number, to decide whether to create an array or an object
+        const nextKeyIsArray = /^\d+$/.test(keys[i + 1]);
+        currentObj[currentKey] = nextKeyIsArray ? [] : {};
+      }
+    }
+    currentObj = currentObj[currentKey];
   }
-  return _object;
+
+  if (value !== undefined) {
+    currentObj[keys[keys.length - 1]] = value;
+  } else {
+    let result = obj;
+    for (const currentKey of keys) {
+      if (!result || typeof result !== "object") {
+        return undefined;
+      }
+      result = result[currentKey];
+    }
+    return result;
+  }
 }
