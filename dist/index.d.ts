09ef0165622c0c2e75856aea2e8f501f845cef39
tamb <t.saporito@gmail.com>
2024-02-17 09:02:52 -0500

    Adds more tests

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,2 @@
-/**
- *
- * Thank you, Ray for the original Stackoverflow answer, which is the majority of the source code.
- * https://stackoverflow.com/questions/6491463/accessing-nested-javascript-objects-with-string-key
- * Check out his github and so profile below:
- * https://github.com/raybellis
- * https://stackoverflow.com/users/6782/alnitak
- *
- */
-export default function byString(object: object, string: string, value?: any): any;
+export default function byString(obj: any, key: string, value?: any): any;
 //# sourceMappingURL=index.d.ts.map
