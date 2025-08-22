4be09dae23c73262738505952a6b8ec064a44b41
Tamb <t.saporito@gmail.com>
2025-08-21 21:45:41 -0400

    removes dist from git

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export default function byString(obj: any, key: string, value?: any): any;
-//# sourceMappingURL=index.d.ts.map
--- dist/original.d.ts
+++
@@ -1,2 +0,0 @@
-export default function byStringOriginal(obj: any, key: string, value?: any): any;
-//# sourceMappingURL=original.d.ts.map
